7619f61d7c89f59f65a112238384a0f7ff991056
Carlos Dante <31288453+CarlosQuinteros@users.noreply.github.com>
2021-07-13 11:41:07 -0300

    Creacion del proyecto

---
+++ Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/main/java/backendAdministradorCompetenciasFutbolisticas/BackendAdministradorCompetenciasFutbolisticasApplication.java
@@ -0,0 +1,13 @@
+package backendAdministradorCompetenciasFutbolisticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdministradorCompetenciasFutbolisticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAdministradorCompetenciasFutbolisticasApplication.class, args);
+	}
+
+}
---
+++ Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/test/java/backendAdministradorCompetenciasFutbolisticas/BackendAdministradorCompetenciasFutbolisticasApplicationTests.java
@@ -0,0 +1,13 @@
+package backendAdministradorCompetenciasFutbolisticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdministradorCompetenciasFutbolisticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
