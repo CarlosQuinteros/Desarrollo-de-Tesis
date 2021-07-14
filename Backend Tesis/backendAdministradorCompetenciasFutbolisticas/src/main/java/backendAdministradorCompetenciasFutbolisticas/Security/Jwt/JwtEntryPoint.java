9cafc375ef4065d6639776f953e48518f34f1109
Carlos Dante <31288453+CarlosQuinteros@users.noreply.github.com>
2021-07-14 17:12:05 -0300

    Implementacion clases Jwt

--- Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/main/java/backendAdministradorCompetenciasFutbolisticas/Security/Jwt/JwtEntryPoint.java
+++ Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/main/java/backendAdministradorCompetenciasFutbolisticas/Security/Jwt/JwtEntryPoint.java
@@ -1,4 +1,25 @@
 package backendAdministradorCompetenciasFutbolisticas.Security.Jwt;
 
-public class JwtEntryPoint {
+import com.sun.deploy.net.HttpResponse;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//comprueba si hay un token valida, de lo contrario devuelve una respuesta 401 no autorizado
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+    private final static Logger logger  = LoggerFactory.getLogger(JwtEntryPoint.class);
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException, ServletException {
+        logger.error("Faild en el metodo commence"); //esto solo se usa en desarrollo para ver cual es el metodo que tira error
+        res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No autorizado");
+    }
 }
--- Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/main/java/backendAdministradorCompetenciasFutbolisticas/Security/Jwt/JwtProvider.java
+++ Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/main/java/backendAdministradorCompetenciasFutbolisticas/Security/Jwt/JwtProvider.java
@@ -1,4 +1,55 @@
 package backendAdministradorCompetenciasFutbolisticas.Security.Jwt;
 
+import backendAdministradorCompetenciasFutbolisticas.Security.Entity.UsuarioPrincipal;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+//esta clase genera el token, tambien tiene un metodo de validacion para comprobar que el token este bien formado y no expirado
+@Component
 public class JwtProvider {
+    private final static Logger logger  = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    public String generateToken(Authentication authentication){
+        UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
+        return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String getNombreUsuarioFromToken(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Token mal formado");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Token no soportado");
+        } catch (ExpiredJwtException e) {
+            logger.error("Token expirado");
+        } catch (IllegalArgumentException e) {
+            logger.error("Token vacío");
+        } catch (SignatureException e) {
+            logger.error("Fallo en la firma");
+        }
+        return false;
+    }
 }
