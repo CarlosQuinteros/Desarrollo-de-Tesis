c5548adcc13cf334bad6623038c44e61566bef0a
Carlos Dante <31288453+CarlosQuinteros@users.noreply.github.com>
2021-07-14 17:55:08 -0300

    Implementacion dto de seguridad

--- Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/main/java/backendAdministradorCompetenciasFutbolisticas/Security/Controller/UsuarioController.java
+++ Backend Tesis/backendAdministradorCompetenciasFutbolisticas/src/main/java/backendAdministradorCompetenciasFutbolisticas/Security/Controller/UsuarioController.java
@@ -1,4 +1,7 @@
 package backendAdministradorCompetenciasFutbolisticas.Security.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
 }
